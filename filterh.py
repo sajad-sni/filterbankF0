dc996d3e2b5e03e1f3253feff9b40fd1817d6473
Sajad <sajad.skhani@gmail.com>
2024-07-08 20:30:04 -0400

    filterh updated. Now it gives harmonics individually as well all harmonic sum.

--- filterh.py
+++ filterh.py
@@ -6,16 +6,20 @@ import matplotlib.pyplot as plt
 
 def filterh(winlen, fs, k, f0min, f0max, nfft):
     win = scipy.signal.windows.hamming(winlen)
-    h = np.zeros((f0max - f0min + 1, nfft // 2+1))
-    h = np.zeros((f0max - f0min + 1, nfft // 2))
+    h = np.zeros((k+1, f0max - f0min + 1, nfft // 2+1))
+    h = np.zeros((k+1, f0max - f0min + 1, nfft // 2))
     t = np.arange(0, winlen/1000, 1/fs)
     t = np.arange(0, winlen)/fs
     norm = 0
     for i, f in enumerate(range(f0min, f0max)):
         s = 0
-        for har in range(1, k+1):
-            #s += np.sin(2*np.pi*har*f*t)
-            s += np.exp(1j*2*np.pi*har*f*t)
+        for har_idx, har in enumerate(range(1, k+1)):
+            #harmonic = np.sin(2*np.pi*har*f*t)
+            harmonic = np.exp(1j*2*np.pi*har*f*t)
+            har_fft = np.abs(np.fft.fft(harmonic, nfft)[:nfft//2])
+            har_fft[1:] = har_fft[1:]*2
+            h[har_idx, f-f0min, :] = har_fft
+            s += harmonic
         # s0, fx0 = F.fft_onesided(s, fs, nfft)
         s = s/k
         sf = np.fft.fft(s, nfft)[:nfft//2]
@@ -23,7 +27,7 @@ def filterh(winlen, fs, k, f0min, f0max, nfft):
         sf = np.abs(sf)
         sf[1:] = sf[1:]*2
         norm = (i * norm + np.max(sf)) / (i + 1)
-        h[f-f0min, :] = sf
+        h[-1, f-f0min, :] = sf
     h[h < 90] = 0
     return h, fx, 1/norm
 
